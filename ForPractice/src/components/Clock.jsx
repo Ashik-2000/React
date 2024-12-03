c05eff84d4a6889300e59f434b55f5ac604abd7a
Ashikul Islam <ashikulislamfb@gmail.com>
2024-12-03 23:15:46 +0600

    bla bla

--- ForPractice/src/components/Clock.jsx
+++ ForPractice/src/components/Clock.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {date: new Date()}
-    }
+class Clock extends React.Component {
+    state = { date: new Date(), local: 'bn-BD' }
 
-
-    componentDidMount(){
-        this.clockTimer = setInterval(() => this.setState({date: new Date()}), 1000);
+    componentDidMount() {
+        this.clockTimer = setInterval(() => {
+            this.setState({ date: new Date() })
+        }, 1000);
     }
 
     // If we go to other pages then this function will be called and it would stop the interval while we are away. When we come back then the 'componentDidMount()' will be called again.
@@ -16,14 +14,22 @@ class Clock extends React.Component{
         clearInterval(this.clockTimer);
     }
 
+    handleClick = () => { 
+        this.setState({local: 'en-US'});
+    }
+
     render() {
+        const {date, local} = this.state;
         return (
-            <h1>
-                <span>
-                    {/* {this.state.date.toLocaleTimeString(this.props.locale)} */}
-                    {this.state.date.toLocaleTimeString()}
-                </span> 
-            </h1>
+            <div>
+                <h1>
+                    <span>
+                        {/* {this.state.date.toLocaleTimeString(this.props.locale)} */}
+                        {date.toLocaleTimeString(local)}
+                    </span>
+                </h1>
+                <button onClick={this.handleClick}>Click Here</button>
+            </div>
         )
     }
 }
