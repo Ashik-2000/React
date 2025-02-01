787488d0f5844845ea1a70d84e42c1189c8e8fe5
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-01 19:54:25 +0600

    components and props

--- ForTutorial/src/App.jsx
+++ ForTutorial/src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
-  
-  return (
-    <>
-      Hellow World
-    </>
-  );
+import React from "react";
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <>
+        <h1>
+          <span>Hello {new Date().toLocaleTimeString(this.props.en)}</span>
+        </h1>
+      </>
+    );
+  }
 }
 
 export default App;
