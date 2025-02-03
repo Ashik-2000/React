78cf7583006d07490e18ce984dce41dc5668b1a7
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-03 13:28:05 +0600

    event Listener

--- ForTutorial/src/components/Clock.jsx
+++ ForTutorial/src/components/Clock.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-class App extends React.Component {
-    state = { date: new Date() };
+class Clock extends React.Component {
+    state = { date: new Date(), locale: "bn-BD" };
     componentDidMount() {
         this.clockTimer = setInterval(() => this.tick(), 1000);
     }
@@ -13,13 +13,18 @@ class App extends React.Component {
             { date: new Date() }
         )
     }
+    handleClick = () => {
+        this.setState({ locale: "en-US" })
+    }
     render() {
+        const { date, locale } = this.state;
         return (
             <>
-                <h1>Time: {this.state.date.toLocaleTimeString(this.props.en)}</h1>
+                <h1>Time: {date.toLocaleTimeString(locale)}</h1>
+                <button onClick={this.handleClick}>Click here</button>
             </>
         );
     }
 }
 
-export default App;
+export default Clock;
