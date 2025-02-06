b95932c39c8dd54e815d160a4b598b487ac3d03d
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-06 19:26:56 +0600

    select and checkbox

--- ForTutorial/src/App.jsx
+++ ForTutorial/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Form from "./components/form";
 
 function App() {
-    return <div>
-        <Form />
-    </div>;
+    return (
+        <div>
+            <Form />
+        </div>
+    );
 }
 
 export default App;
--- ForTutorial/src/components/Form.jsx
+++ ForTutorial/src/components/Form.jsx
@@ -3,14 +3,31 @@ import React from "react";
 export default class Form extends React.Component {
     state = {
         title: "javaScript",
+        text: "JavaScript is awesome.",
+        library: "react",
+        isAwesome: true,
     };
     handleChange = (event) => {
-        this.setState({
-            title: event.target.value,
-        });
-    }
+        if (event.target.type === "text") {
+            this.setState({
+                title: event.target.value,
+            });
+        } else if (event.target.type === "textarea") {
+            this.setState({
+                text: event.target.value,
+            });
+        } else if (event.target.type === "select-one") {
+            this.setState({
+                library: event.target.value,
+            });
+        } else if (event.target.type === "checkbox") {
+            this.setState({
+                isAwesome: event.target.checked,
+            });
+        }
+    };
     render() {
-        const { title } = this.state;
+        const { title, text, library, isAwesome } = this.state;
         return (
             <>
                 <form>
@@ -20,6 +37,22 @@ export default class Form extends React.Component {
                         value={title}
                         onChange={this.handleChange}
                     />
+                    <br />
+                    <br />
+                    <textarea
+                        name="text"
+                        value={text}
+                        onChange={this.handleChange}
+                    ></textarea>
+                    <br />
+                    <br />
+                    <select value={library} onChange={this.handleChange}>
+                        <option value="react">react</option>
+                        <option value="angular">angular</option>
+                    </select>
+                    <br />
+                    <br />
+                    <input type="checkbox" checked={isAwesome} onChange={this.handleChange}/>
                 </form>
             </>
         );
