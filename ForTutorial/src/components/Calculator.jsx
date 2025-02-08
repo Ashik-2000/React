f60590d6a705fd17c4903f6c8d1c95ddbade2f71
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-08 12:27:53 +0600

    lib

--- ForTutorial/src/components/Calculator.jsx
+++ ForTutorial/src/components/Calculator.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import TemperatureInput from "./TemleratureInput";
+import { convert, toCelsius, toFahrenheit } from "../lib/converter";
+import BoillongVerdict from "./BoillingVerdict";
 
 export default class Calculator extends React.Component {
+    state = { temperature: "", scale: "" };
+    handleChange = (event, scale) => {
+        this.setState({
+            temperature: event.target.value,
+            scale,
+        });
+    };
     render() {
+        const { temperature, scale } = this.state;
+        const celsius =
+            scale === "f" ? convert(temperature, toCelsius) : temperature;
+        const fahrenheit =
+            scale === "c" ? convert(temperature, toFahrenheit) : temperature;
         return (
             <>
-                <TemperatureInput scale='c'/>
-                <TemperatureInput scale='f'/> 
-                {/* <BoillingVerdict celsius={parseFloat(temperature)} /> */}
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleChange}
+                />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleChange}
+                />
+                <BoillongVerdict celsius={parseFloat(temperature)} />
             </>
         );
     }
