e48727374bbcf27ff32931d01ab051a1e611cd04
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-09 23:41:10 +0600

    render props final

--- ForTutorial2/src/App.jsx
+++ ForTutorial2/src/App.jsx
@@ -1,11 +1,26 @@
 import ClickCounter from "./components/RenderProps/ClickCounter";
 import HoverCounter from "./components/RenderProps/HoverCounter";
+import Counter from "./components/RenderProps/Counter";
 
 export default function App() {
     return (
         <>
-            <HoverCounter />
-            <ClickCounter />
+            <Counter
+                render={(count, incrementCount) => (
+                    <HoverCounter
+                        count={count}
+                        incrementCount={incrementCount}
+                    />
+                )}
+            />
+            <Counter
+                render={(count, incrementCount) => (
+                    <ClickCounter
+                        count={count}
+                        incrementCount={incrementCount}
+                    />
+                )}
+            />
         </>
     );
 }
--- ForTutorial2/src/components/RenderProps/ClickCounter.jsx
+++ ForTutorial2/src/components/RenderProps/ClickCounter.jsx
@@ -1,18 +1,7 @@
-import React from "react";
-
-export default class ClickCounter extends React.Component {
-    state = {
-        count: 0,
-    };
-    incrementCount = () => {
-        this.setState((state) => ({ count: state.count + 1 }));
-    };
-    render() {
-        const { count } = this.state;
-        return (
-            <>
-                <button onClick={this.incrementCount}>Click {count} times</button>
-            </>
-        );
-    }
+export default function ClickCounter({ count, incrementCount }) {
+    return (
+        <>
+            <button onClick={incrementCount}>Click {count} times</button>
+        </>
+    );
 }
