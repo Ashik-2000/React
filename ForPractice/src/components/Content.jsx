dfa533fc43eed2782b68400e78919a709907f8c4
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-16 21:36:03 +0600

    ContextAPI

--- ForPractice/src/components/Content.jsx
+++ ForPractice/src/components/Content.jsx
@@ -1,23 +1,22 @@
+import { useContext } from "react";
 import Counter from "./Counter";
 import HoverCounter from "./HoverCounter";
 import ThemeContext from "../contexts/themeContext";
 
 export default function Content() {
+    const context = useContext(ThemeContext);
+    const { theme, switchTheme } = context;
     return (
         <>
             <h1>This is content component</h1>
             <Counter>
                 {(count, incrementCount) => (
-                    <ThemeContext.Consumer>
-                        {({ theme, switchTheme }) => (
-                            <HoverCounter
-                                count={count}
-                                incrementCount={incrementCount}
-                                theme={theme}
-                                switchTheme={switchTheme}
-                            />
-                        )}
-                    </ThemeContext.Consumer>
+                    <HoverCounter
+                        count={count}
+                        incrementCount={incrementCount}
+                        theme={theme}
+                        switchTheme={switchTheme}
+                    />
                 )}
             </Counter>
         </>
