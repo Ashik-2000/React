20b1afd9e4ca69e29c324ec0a1b1969488269557
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-17 13:57:59 +0600

    React Hook

--- ForPractice/src/App.jsx
+++ ForPractice/src/App.jsx
@@ -1,38 +1,12 @@
 import React from "react";
-import Counter from "./components/Counter";
-import ClickCounter from "./components/ClickCounter";
-import Section from "./components/Section";
-import ThemeContext from "./contexts/themeContext";
+import Todo from "./components/TodoClass";
 
-class App extends React.Component {
-    state = {
-        theme: "dark",
-    };
-    switchTheme = () => {
-        this.setState((prevState) => ({
-            theme: prevState.theme === "dark" ? "light" : "dark",
-        }));
-    };
+export default class App extends React.Component {
     render() {
-        const { theme } = this.state;
         return (
-            <>
-                <Counter>
-                    {(count, incrementCount) => (
-                        <ClickCounter
-                            count={count}
-                            incrementCount={incrementCount}
-                        />
-                    )}
-                </Counter>
-                <ThemeContext.Provider
-                    value={{ theme: theme, switchTheme: this.switchTheme }}
-                >
-                    <Section />
-                </ThemeContext.Provider>
-            </>
+            <div className="app">
+                <Todo />
+            </div>
         );
     }
 }
-
-export default App;
--- ForPractice/src/components/ClickCounter.jsx
+++
@@ -1,9 +0,0 @@
-function ClickCounter({ count, incrementCount }) {
-    return (
-        <>
-            <button onClick={incrementCount}>Clicked {count} times</button>
-        </>
-    );
-}
-
-export default ClickCounter;
