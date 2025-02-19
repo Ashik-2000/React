651140186362b7e857887b53f331f5e414800ec5
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-19 13:35:41 +0600

    useEffect

--- ForPractice/src/App.jsx
+++ ForPractice/src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import MyComponent from "./components/ClassComponent";
 import MyFunctionComponent from "./components/FunctionalComponent";
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                {/* <MyComponent /> */}
-                <MyFunctionComponent />
+export default function App() {
+    const [show, setShow] = useState(true);
+    return (
+        <div className="app">
+            {/* <MyComponent /> */}
+            <div>
+                {show && <MyFunctionComponent />}
             </div>
-        );
-    }
+            <button
+                type="button"
+                onClick={() => setShow((prevSho) => !prevSho)}
+            >
+                {show ? "Hide" : "Show"}
+            </button>
+        </div>
+    );
 }
--- ForPractice/src/components/FunctionalComponent.jsx
+++ ForPractice/src/components/FunctionalComponent.jsx
@@ -2,19 +2,27 @@ import { useState, useEffect } from "react";
 
 export default function MyFunctionComponent() {
     const [count, setCount] = useState(0);
-    // const [date, setDate] = useState(new Date());
-    // const Tick = () => {
-    //     setDate(new Date());
-    // };
+    const [date, setDate] = useState(new Date());
+    const Tick = () => {
+        console.log("Click ticking");
+        setDate(new Date());
+    };
     useEffect(() => {
+        console.log("Updating document title.");
         document.title = `Clicked ${count} times`;
-    });
+    }, [count]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            Tick();
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
     const addClick = () => {
         setCount((count) => count + 1);
     };
     return (
         <>
-            {/* <p>Time: {time.toLocaleTimeString()}</p> */}
+            <p>Time: {date.toLocaleTimeString()}</p>
             <button type="button" onClick={addClick}>
                 Click Here
             </button>
