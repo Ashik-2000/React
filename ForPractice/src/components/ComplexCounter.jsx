44a26675da3f850408b2926ea2991ed894366889
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-27 01:23:41 +0600

    spread operator

--- ForPractice/src/components/ComplexCounter.jsx
+++ ForPractice/src/components/ComplexCounter.jsx
@@ -1,12 +1,16 @@
 import { useReducer } from "react";
 
-const initialState = { counter: 0, counter2:0 };
+const initialState = { counter: 0, counter2: 0 };
 const reducer = (state, action) => {
     switch (action.type) {
         case "increment":
-            return { counter: state.counter + action.value };
+            return { ...state, counter: state.counter + action.value };
         case "decrement":
-            return { counter: state.counter + action.value };
+            return { ...state, counter: state.counter - action.value };
+        case "increment2":
+            return { ...state, counter2: state.counter2 + action.value };
+        case "decrement2":
+            return { ...state, counter2: state.counter2 - action.value };
         default:
             return state;
     }
@@ -26,22 +30,22 @@ export default function Counter() {
                 </button>
                 <button
                     type="button"
-                    onClick={() => dispatch({ type: "decrement", value: -1 })}
+                    onClick={() => dispatch({ type: "decrement", value: 1 })}
                 >
                     Decrement by 1
                 </button>
             </div>
             <div>
-                <div>Count = {count.counter}</div>
+                <div>Count2 = {count.counter2}</div>
                 <button
                     type="button"
-                    onClick={() => dispatch({ type: "increment", value: 1 })}
+                    onClick={() => dispatch({ type: "increment2", value: 1 })}
                 >
                     Increment by 1
                 </button>
                 <button
                     type="button"
-                    onClick={() => dispatch({ type: "decrement", value: -1 })}
+                    onClick={() => dispatch({ type: "decrement2", value: 1 })}
                 >
                     Decrement by 1
                 </button>
