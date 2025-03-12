206b2604e0187f6a020f538312f721747470c6dd
Ashikul Islam <ashikulislamfb@gmail.com>
2025-03-12 12:51:10 +0600

    style sheet

--- ForHookTutorial/src/useReduce/app.jsx
+++ ForHookTutorial/src/useReduce/app.jsx
@@ -1,1 +1,31 @@
-// Copy the highlighted codes into the App.jsx
+// Copy the commneted codes into the App.jsx
+// import React from "react";
+// import { useReducer } from "react";
+// import ComponentA from "./useReduce/ComponentA";
+
+// export const counterContext = React.createContext();
+
+// const initialValue = 0;
+// const reducer = (state, action) => {
+//     switch (action) {
+//         case "increment":
+//             return state + 1;
+//         case "decrement":
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// };
+function App() {
+    // const [count, dispatch] = useReducer(reducer, initialValue);
+    return (
+        <>
+            {/* <h1>Count: {count}</h1>
+            <counterContext.Provider value={{dispatch: dispatch}}>
+                <ComponentA />
+            </counterContext.Provider> */}
+        </>
+    );
+}
+
+export default App;
--- ForPractice/src/App.jsx
+++ ForPractice/src/App.jsx
@@ -1,29 +1,9 @@
-import { useReducer } from "react";
-import React from "react";
-import ComponentA from "./components/ComponentA";
-
-export const counterContext = React.createContext();
-
-const initialState = 0;
-const reducer = (state, action) => {
-    switch (action) {
-        case "increment":
-            return state + 1;
-        case "decrement":
-            return state - 1;
-        default:
-            return state;
-    }
-};
-
+import Logo from "./components/Logo";
+import "./assets/CSS/global.css";
 export default function App() {
-    const [count, dispatch] = useReducer(reducer, initialState);
     return (
-        <div className="app">
-            <h1>Count: {count}</h1>
-            <counterContext.Provider value={{ dispatch: dispatch }}>
-                <ComponentA />
-            </counterContext.Provider>
+        <div className="App">
+            <Logo />
         </div>
     );
 }
