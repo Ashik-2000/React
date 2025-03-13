0504c1d981716d55be76e0b3305ca1a415f2caa8
Ashikul Islam <ashikulislamfb@gmail.com>
2025-03-13 13:11:10 +0600

    style

---
+++ ForStyling/src/App.jsx
@@ -0,0 +1,10 @@
+import GlobalStyle from "./components/styles/Global.styles";
+import Tutorial from "./components/Tutorial";
+export default function App() {
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <Tutorial />
+        </div>
+    );
+}
