d37243d2a35e5ea3e59d7dd4806f9c9c1fb7c3e0
Ashikul Islam <ashikulislamfb@gmail.com>
2025-03-14 19:44:05 +0600

    Nested Routing

--- ForPractice/src/App.jsx
+++ ForPractice/src/App.jsx
@@ -1,19 +1,22 @@
-import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Document from "./pages/Document";
 import News from "./pages/News";
+import ReactLayouts from "./layouts/ReactLayout";
+import "./assets/main.css";
 
 export default function App() {
     return (
         <div className="App">
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/document" element={<Document />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/" element={<ReactLayouts />}>
+                    <Route path="home" element={<Home />} />
+                    <Route path="document" element={<Document />} />
+                    <Route path="news" element={<News />} />
+                    <Route path="about" element={<About />} />
+                </Route>
             </Routes>
         </div>
     );
--- ForPractice/src/components/Navbar.jsx
+++ ForPractice/src/components/Navbar.jsx
@@ -1,21 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import "../assets/navbar.css";
+
 export default function Navbar() {
+    const navigate = useNavigate();
     return (
         <>
-            <div>
-                <Link to="/">
-                    <li>Home</li>
-                </Link>
-                <Link to="/document">
-                    <li>Document</li>
-                </Link>
-                <Link to="/news">
-                    <li>News</li>
-                </Link>
-                <Link to="/about">
-                    <li>About</li>
-                </Link>
-            </div>
+            <nav className="navbar">
+                <ul>
+                    <li>
+                        <NavLink to="/home">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/document">Document</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/news">News</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <button
+                        onClick={() => navigate("/home", { replace: true })}
+                    >
+                        Back to home
+                    </button>
+                </ul>
+            </nav>
         </>
     );
 }
