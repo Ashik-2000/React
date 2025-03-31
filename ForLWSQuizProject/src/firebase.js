a3a5ced078ea3f7810bf72cb3eeaeed48a0b6e3e
Ashikul Islam <ashikulislamfb@gmail.com>
2025-03-31 17:16:24 +0600

    realtime databse env variable

--- ForLWSQuizProject/src/firebase.js
+++ ForLWSQuizProject/src/firebase.js
@@ -4,10 +4,11 @@ import { initializeApp } from "firebase/app";
 const app = initializeApp({
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID, // Fixed typo: API_ID → APP_ID
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 export default app;
