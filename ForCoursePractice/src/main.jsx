7492a54da5f67ef88a6ca81d0cd8b6550011f5bf
Ashikul Islam <ashikulislamfb@gmail.com>
2025-06-23 13:28:41 +0600

    Context Passing

--- ForCoursePractice/src/main.jsx
+++ ForCoursePractice/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
+import Page from "./contextapi/Page";
 import "./style.css";
-import TaskUseState from "./useReducer/TaskReducer";
+// import TaskUseState from "./useReducer/TaskReducer";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <TaskUseState />
+        <Page />
     </StrictMode>
 );
