06ab67ddf91c9d1b1a1c6484f27d0b0773d72314
Ashikul Islam <ashikulislamfb@gmail.com>
2025-07-16 18:41:56 +0600

    post and error handling

--- AXIOS/src/App.jsx
+++ AXIOS/src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import AddPost from "./components/AddPost.jsx";
 import EditPost from "./components/EditPost.jsx";
 import Posts from "./components/Posts.jsx";
-import initialPosts from "./data/db.js";
 
 function App() {
-    const [posts, setPosts] = useState(initialPosts);
+    const [posts, setPosts] = useState([]);
     const [post, setPost] = useState(null);
+    const [error, setError] = useState(null);
 
-    const handleAddPost = (newPost) => {
-        const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
-        setPosts([
-            ...posts,
-            {
+    const handleAddPost = async (newPost) => {
+        try {
+            const id = posts.length
+                ? Number(posts[posts.length - 1].id) + 1
+                : 1;
+            const finalPost = {
                 id,
                 ...newPost,
-            },
-        ]);
+            };
+
+            const response = await axios.post(
+                "http://localhost:8000/posts",
+                finalPost
+            );
+
+            setPosts([...posts, response.data]);
+        } catch (error) {
+            if (error.response) {
+                setError(`${error.message} - ${error.response.data}`);
+            } else {
+                setError(error.message);
+            }
+        }
     };
 
     const handleDeletePost = (postId) => {
@@ -35,6 +50,26 @@ function App() {
         setPosts(updatedPosts);
     };
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/posts");
+                if (response && response.data) {
+                    setPosts(response.data);
+                }
+            } catch (error) {
+                console.log(error);
+                if (error.response) {
+                    setError(`${error.message} - ${error.response.data}`);
+                } else {
+                    setError(error.message);
+                }
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
     return (
         <div>
             <div>
@@ -51,6 +86,12 @@ function App() {
                         <AddPost onAddPost={handleAddPost} />
                     ) : (
                         <EditPost post={post} onEditPost={handleEditPost} />
+                    )}
+                    {error && (
+                        <>
+                            <hr />
+                            <div className="error">{error}</div>
+                        </>
                     )}
                 </div>
             </div>
